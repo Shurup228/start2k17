7d2dbe40118ab290189ce0c60d7a4fa5ce48beb9
Shurup228 <yroslav541@gmail.com>
2017-03-30 00:52:09 +0300

    Added description for layout

--- gui/layout.py
+++ gui/layout.py
@@ -1,16 +1,18 @@
 """Layout like layouts in qt(QGridLayout)."""
 # coding=utf-8
 
-DUMMY = ''
-
 
 class Layout:
+    """Grid layout for managing simple menus."""
+
+    # Dummy item
+    DUMMY = ''
+
     def __init__(self, scene):
         self.__scene = scene
         self.width, self.height = scene.width(), scene.height()
-        self.rectWidth, self.rectHeight = self.width, self.height
-        self.rows = 1
-        self.colls = 1
+        self.rectWidth, self.rectHeight = None, None
+        self.rows, self.colls = 1, 1
         # items - matrix with elements
         self.items = [[]]
 
@@ -19,29 +21,31 @@ class Layout:
         self.colls = self.colls if coll + 1 <= self.colls else coll + 1
         self.resize()
 
-        if item != DUMMY:
+        if item != self.DUMMY:
             self.__scene.addItem(item)
 
         self.items[row][coll] = item
         self.repaint()
 
     def resize(self):
+        """Fill item matrix to fit in all colls and rows."""
         for x in range(self.rows + 1 - len(self.items)):
             self.items.append([])
 
         for row in range(len(self.items)):
             for y in range(self.colls - len(self.items[row])):
-                self.items[row].append('')
+                self.items[row].append(self.DUMMY)
 
         self.rectWidth = self.width / self.colls
         self.rectHeight = self.height / self.rows
 
     def repaint(self):
+        """Redraws all widgets after new added and matrix resized."""
         for row in range(self.rows):
             for coll in range(self.colls):
                 item = self.items[row][coll]
 
-                if item == '':
+                if item == self.DUMMY:
                     continue
 
                 rect = item.boundingRect()
